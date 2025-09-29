1fba73105474a7f3758771bcf7212dfb28bcb929
LEANHDUC2005 <leanhduc350@gmail.com>
2025-09-29 21:26:36 +0700

    new problem

---
+++ Polling & Swap/Remove Duplicates from Sorted Array.c
@@ -0,0 +1,89 @@
+/* 26. Remove Duplicates from Sorted Array
+
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order. */
+// Hashing  :
+int removeDuplicates(int* nums, int numsSize) {
+    int hashmap[201];
+    int offset = 100;
+    int value;
+
+    memset( hashmap, -1, sizeof(hashmap) );
+
+    for(int i = 0; i < numsSize; i++ )
+    {
+        value = nums[i] + offset;
+        if ( hashmap[value] == -1 )
+        {
+            hashmap[value] = i;
+        }
+    }
+    
+    int count;
+    int j = 0;
+
+    for(int i = 0; i < 201 ; i++ )
+    {
+        if ( hashmap[i] != -1 )
+        {
+            nums[j] = i - offset ;
+            j++;
+        }
+    }
+    return j;
+}
+// Polling and Overwriting 
+int removeDuplicates(int* nums, int numsSize) {
+    int check;
+    int act = 0;
+
+    for( check = 1; check < numsSize; check++ )
+    {
+        if ( nums[check] != nums[act] )
+        {
+            act++;
+            nums[act] = nums[check];
+        }
+    }
+    return act + 1;
+}
