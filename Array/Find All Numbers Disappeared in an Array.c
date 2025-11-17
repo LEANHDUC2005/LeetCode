1bb78f74540dfee39f8d37d81ea4f9b92e561aca
Lê Anh Đức <leanhduc350@gmail.com>
2025-11-18 01:27:03 +0700

    Create Find All Numbers Disappeared in an Array.c

---
+++ Array/Find All Numbers Disappeared in an Array.c
@@ -0,0 +1,25 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
+    //int y = 0;
+    for(int i=0; i<numsSize; i++)
+    {
+        // y = abs(nums[i]) - 1
+        if ( nums[abs(nums[i]) - 1] > 0 ) nums[abs(nums[i]) - 1] = - nums[abs(nums[i]) - 1];
+    }
+
+    int j = 0;
+    int *return_arr = (int*)malloc(sizeof(int) * numsSize);
+    
+    for(int i=0; i<numsSize; i++)
+    {
+        if ( nums[i] > 0 ) 
+        {
+            return_arr[j++] = i + 1;
+        }
+    }
+
+    *returnSize = j;
+    return return_arr;
+}
