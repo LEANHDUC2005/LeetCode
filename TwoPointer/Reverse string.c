acaacdfd6f5183a1c840d6338d76baf1753430de
Lê Anh Đức <leanhduc350@gmail.com>
2025-12-28 09:30:10 +0700

    Update Reverse string.c

--- TwoPointer/Reverse string.c
+++ TwoPointer/Reverse string.c
@@ -9,3 +9,17 @@ void reverseString(char* s, int sSize) {
         --right;
     }
 }
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    for(int i=0; i < nums2Size; i++){
+        int j=0;
+        while( nums2[i] > nums1[j] && j < m ){
+            ++j;
+        }
+        for(int t=m-1; t >= j; t--){
+            nums1[t+1] = nums1[t];
+        }
+        nums1[j] = nums2[i];
+        m++;
+    }
+}
